afaf2af8bdedfe6d1a274dff97f6208e927345aa
ivysot <vitorissamu.vs@gmail.com>
2021-07-28 14:31:17 -0300

    first commit

---
+++ src/main/java/project/ApiDio/gameapi/GameapiApplication.java
@@ -0,0 +1,13 @@
+package project.ApiDio.gameapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/project/ApiDio/gameapi/GameapiApplicationTests.java
@@ -0,0 +1,13 @@
+package project.ApiDio.gameapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
